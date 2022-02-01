ec1a1c843692b33dc10c2c3440720c1a700099f2
aweaver <aweaver@student.42.fr>
2022-02-01 16:39:26 +0100

    adding maintest

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int main(void)
 
 	mlx = mlx_init();
 	mlx_window = mlx_new_window(mlx, 500, 500, "My custom window");
+	mlx_pixel_put(mlx, mlx_window, 250, 250, 0x00ffffff);
+	mlx_key_hook(mlx_window, &mlx_clear_window, 27);
 	mlx_loop(mlx);
+	//mlx_key_hook(mlx_window, &mlx_close_window, 27);
 	return (0);
 }
