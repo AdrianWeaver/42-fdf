27841363d8a4586fa68e5fdf7802f40c6495780c
aweaver <aweaver@student.42.fr>
2022-02-25 14:59:14 +0100

    switched var to stack memory to avoid leak and useless heap allocation

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 07:16:23 by aweaver           #+#    #+#             */
-/*   Updated: 2022/02/25 11:50:26 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/02/25 14:58:29 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,21 @@ static void	ft_make_square(t_fdf_env *env, t_fdf_img *img)
 	}
 }
 
-void	ft_init_var(t_fdf_env *env)
+void	ft_init_var(t_fdf_env *env, t_fdf_var *var)
 {
-	t_fdf_var	*var;
-
-	var = malloc(sizeof(*var) * 1);
 	var->mod_height = 5;
 	var->spread = 10;
 	var->angle = 0.52;
 	env->var = var;
 }
 
-void	ft_init_img(t_fdf_env *env, t_fdf_img *img)
+void	ft_init_img(t_fdf_env *env, t_fdf_img *img, t_fdf_var *var)
 {
 	img->img_id = mlx_new_image(env->mlx_id, env->window_w, env->window_h);
 	img->img_str = mlx_get_data_addr(img->img_id, &img->bits_per_pixel,
 			&img->line_length, &img->endian);
 	env->img = img;
-	ft_init_var(env);
+	ft_init_var(env, var);
 }
 
 void	t_fdf_init_env(t_fdf_env *env)
@@ -72,11 +69,9 @@ int	main(void)
 {
 	t_fdf_env	env;
 	t_fdf_img	img;
+	t_fdf_var	var;
 
 	t_fdf_init_env(&env);
-	//env.window_w = 500;
-	//env.window_h = 500;
-	//env.mlx_id = mlx_init();
 	if (env.mlx_id == 0)
 	{
 		ft_printf("%s\n", MLX_FAILURE);
@@ -86,14 +81,11 @@ int	main(void)
 			"Don't judge me I'm an artist");
 	if (env.mlx_window == 0)
 		return (0);
-	ft_init_img(&env, &img);
+	ft_init_img(&env, &img, &var);
 	ft_fdf_open_map("test_maps/42.fdf", &env);
 	ft_draw_map(&env);
 	ft_make_square(&env, &img);
 	ft_put_pixel_img(&img, 250, 250, 0xff0000);
-	ft_put_pixel_img(&img, 251, 251, 0xff0000);
-	ft_put_pixel_img(&img, 252, 252, 0xff0000);
-	ft_put_pixel_img(&img, 253, 253, 0xff0000);
 	mlx_put_image_to_window(env.mlx_id, env.mlx_window, img.img_id, 0, 0);
 	mlx_hook(env.mlx_window, 3, 1L << 1, &ft_check_keys, &env);
 	mlx_hook(env.mlx_window, 17, 0, &ft_nuke_program, &env);
