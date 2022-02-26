db57275bf0cbadaf9460660c0137530225c2fa33
aweaver <aweaver@student.42.fr>
2022-02-26 11:43:47 +0100

    WIP : adding bonuses

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 07:16:23 by aweaver           #+#    #+#             */
-/*   Updated: 2022/02/25 17:11:04 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/02/26 11:43:11 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,20 @@
 #include "ft_fdf.h"
 #include "get_next_line.h"
 
-static void	ft_make_square(t_fdf_env *env, t_fdf_img *img)
-{
-	int	x;
-	int	y;
-
-	return ;
-	x = 50;
-	while (x < (env->window_w - 50))
-	{
-		y = 50;
-		while (y < (env->window_h - 50))
-		{
-			ft_put_pixel_img(img, x, y, 0xff0000);
-			y++;
-		}
-		x++;
-	}
-}
-
 void	ft_init_var(t_fdf_env *env, t_fdf_var *var)
 {
-	var->mod_height = 5;
-	var->spread = 20;
+	var->mod_height = 1;
+	var->spread = 10;
 	var->angle = 0.52;
 	env->var = var;
 }
 
-void	ft_init_img(t_fdf_env *env, t_fdf_img *img, t_fdf_var *var)
+void	ft_init_img(t_fdf_env *env, t_fdf_img *img)
 {
 	img->img_id = mlx_new_image(env->mlx_id, env->window_w, env->window_h);
 	img->img_str = mlx_get_data_addr(img->img_id, &img->bits_per_pixel,
 			&img->line_length, &img->endian);
 	env->img = img;
-	ft_init_var(env, var);
 }
 
 void	t_fdf_init_env(t_fdf_env *env)
@@ -65,7 +45,7 @@ void	t_fdf_init_env(t_fdf_env *env)
 	}
 }
 
-int	main(void)
+int	main_2(char **argv)
 {
 	t_fdf_env	env;
 	t_fdf_img	img;
@@ -78,17 +58,23 @@ int	main(void)
 		return (0);
 	}
 	env.mlx_window = mlx_new_window(env.mlx_id, env.window_w, env.window_h,
-			"My first ever NFT");
+			argv[1]);
 	if (env.mlx_window == 0)
 		return (0);
-	ft_init_img(&env, &img, &var);
-	ft_fdf_open_map("test_maps/42.fdf", &env);
+	ft_init_img(&env, &img);
+	ft_init_var(&env, &var);
+	ft_fdf_open_map(argv[1], &env);
 	ft_draw_map(&env);
-	ft_make_square(&env, &img);
-	ft_put_pixel_img(&img, 250, 250, 0xff0000);
 	mlx_put_image_to_window(env.mlx_id, env.mlx_window, img.img_id, 0, 0);
 	mlx_hook(env.mlx_window, 3, 1L << 1, &ft_check_keys, &env);
 	mlx_hook(env.mlx_window, 17, 0, &ft_nuke_program, &env);
 	mlx_loop(env.mlx_id);
-	return (0);
+	exit (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (0);
+	main_2(argv);
 }
