eb2863d20230042d56e7c4630e5e4438642cf537
Adrian WEAVER <aweaver@student.42.fr>
2022-03-01 18:23:09 +0100

    rotation is now fully operational

--- sources/ft_check_keys.c
+++ sources/ft_check_keys.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 08:11:34 by aweaver           #+#    #+#             */
-/*   Updated: 2022/02/28 17:29:31 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/01 18:14:28 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ void	ft_check_rotate(int keycode, t_fdf_env *env)
 	{
 		env->var->angle -= 0.7854;
 		if (env->var->angle <= 0)
-			env->var->angle = 0.7854;
+			env->var->angle = 6.2832;
 		ft_redraw(env);
 	}
 	if (keycode == KEY_SPACE)
 	{
-		env->var->angle = 0;
+		env->var->angle = -0.5236;
 		ft_redraw(env);
 	}
 }
